c465da15ac0f5c7832c1c53cece7266df9698d31
9uy4rt <9uy4rt@gmail.com>
2022-07-14 16:43:25 +0900

    ...

--- app/src/main/java/com/iot/bdos/MainActivity.java
+++ app/src/main/java/com/iot/bdos/MainActivity.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,11 +15,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final String[] country = {"-국가 선택-","한국", "미국", "독일", "중국", "일본", "스페인"};
-        final String[] country2 = {"-국가 선택-","한국", "미국", "독일", "wnd", "일본", "스페인"};
+        final String[] stone1 = {"-광명석 선택-","풀의 광명석:미지", "풀의 광명석:수완", "풀의 광명석:수완", "풀의 광명석:파랑", "오색빛 광명석"};
+        final String[] stone2 = {"-광명석 선택-","풀의 광명석:미지", "풀의 광명석:수완", "풀의 광명석:수완", "풀의 광명석:파랑", "오색빛 광명석"};
+        final String[] stone3 = {"-광명석 선택-","풀의 광명석:미지", "풀의 광명석:수완", "풀의 광명석:수완", "풀의 광명석:파랑", "오색빛 광명석"};
+        final String[] stone4 = {"-광명석 선택-","풀의 광명석:미지", "풀의 광명석:수완", "풀의 광명석:수완", "풀의 광명석:파랑", "오색빛 광명석"};
         Spinner spiner1 = (Spinner) findViewById(R.id.spinner1);
 
-        ArrayAdapter adapter1 = new ArrayAdapter(this, android.R.layout.simple_spinner_item, country);
+        ArrayAdapter adapter1 = new ArrayAdapter(this, android.R.layout.simple_spinner_item, stone1);
         adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spiner1.setAdapter(adapter1);
 
@@ -37,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
         });
         Spinner spiner2 = (Spinner) findViewById(R.id.spinner2);
 
-        ArrayAdapter adapter2 = new ArrayAdapter(this, android.R.layout.simple_spinner_item, country2);
+        ArrayAdapter adapter2 = new ArrayAdapter(this, android.R.layout.simple_spinner_item, stone2);
         adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spiner2.setAdapter(adapter2);
 
@@ -54,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
         });
         Spinner spiner3 = (Spinner) findViewById(R.id.spinner3);
 
-        ArrayAdapter adapter3 = new ArrayAdapter(this, android.R.layout.simple_spinner_item, country);
+        ArrayAdapter adapter3 = new ArrayAdapter(this, android.R.layout.simple_spinner_item, stone3);
         adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spiner3.setAdapter(adapter3);
 
@@ -71,7 +72,7 @@ public class MainActivity extends AppCompatActivity {
         });
         Spinner spiner4 = (Spinner) findViewById(R.id.spinner4);
 
-        ArrayAdapter adapter4 = new ArrayAdapter(this, android.R.layout.simple_spinner_item, country);
+        ArrayAdapter adapter4 = new ArrayAdapter(this, android.R.layout.simple_spinner_item, stone4);
         adapter4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spiner4.setAdapter(adapter4);
 
